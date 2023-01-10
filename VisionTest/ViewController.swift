e5b8290cb4327f462292f18844d4871a50d88c9f
kengo-saito <k.saitoh0526@gmail.com>
2023-01-10 19:18:11 +0900

    Initial Commit

---
+++ VisionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionTest
+//
+//  Created by 齋藤健悟 on 2023/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
