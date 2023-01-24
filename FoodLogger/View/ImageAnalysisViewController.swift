c4fdaba5143ea672ab89fb2e245ba4c693c1bd36
kengo-saito <k.saitoh0526@gmail.com>
2023-01-24 19:50:47 +0900

    add image picker button

--- FoodLogger/View/ImageAnalysisViewController.swift
+++ FoodLogger/View/ImageAnalysisViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Vision
 import VisionKit
+import PhotosUI
 
 class ImageAnalysisViewController: UIViewController {
 
@@ -15,6 +16,16 @@ class ImageAnalysisViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func didPushImageChange(_ sender: Any) {
+        // show image picker
+        var conf = PHPickerConfiguration()
+        conf.filter = .images
+        conf.preferredAssetRepresentationMode = .current
+        let picker = PHPickerViewController(configuration: conf)
+        picker.delegate = self
+        photoAuth(picker: picker)
+    }
     
     
     let analyzer = ImageAnalyzer()
@@ -25,6 +36,42 @@ class ImageAnalysisViewController: UIViewController {
         addDoneButtonToTextView()
         addTextRecognition()
         textView.delegate = self
+    }
+    
+    private func photoAuth(picker: PHPickerViewController) {
+        let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        
+        switch status {
+        case .notDetermined:
+            // 初回起動時アルバムアクセス権限確認
+            PHPhotoLibrary.requestAuthorization { status in
+                switch status {
+                case .authorized:
+                    DispatchQueue.main.async {
+                        self.present(picker, animated: true)
+                    }
+                default:
+                    break
+                }
+            }
+        case .restricted, .authorized, .limited:
+            self.present(picker, animated: true)
+        case .denied:
+            // アクセス権限がないとき
+            let alert = UIAlertController(title: "", message: "写真へのアクセスを許可してください", preferredStyle: .alert)
+            let settingsAction = UIAlertAction(title: "設定", style: .default, handler: { (_) -> Void in
+                guard let settingsURL = URL(string: UIApplication.openSettingsURLString ) else {
+                    return
+                }
+                UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+            })
+            let closeAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+            alert.addAction(settingsAction)
+            alert.addAction(closeAction)
+            self.present(alert, animated: true, completion: nil)
+        @unknown default:
+            fatalError()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -137,6 +184,39 @@ extension ImageAnalysisViewController: UITextViewDelegate {
     
     func textViewDidChange(_ textView: UITextView) {
         self.resultLabel.text = makeWords().reduce("", { $0 + $1 + "/"})
+    }
+    
+}
+
+extension ImageAnalysisViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        guard let itemProvider = results.first?.itemProvider else {
+            return
+        }
+        let typeChecked = itemProvider.registeredTypeIdentifiers.map {
+            itemProvider.hasItemConformingToTypeIdentifier($0)
+        }
+        if typeChecked.contains(false) {
+            return
+        }
+        itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.image.identifier) { url, error in
+            if let error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let url else {
+                return
+            }
+            guard let imageData = try? Data(contentsOf: url) else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: imageData)
+            }
+        }
+        
     }
     
 }
